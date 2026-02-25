d47542f8313f65dd6ad9f1df560ef278fa526917
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-25 09:26:26 -0500

    Supermercado

--- Metodos.java
+++ Metodos.java
@@ -21,36 +21,14 @@ public class Metodos {
         }
     }
 
-    public Obj[][] LlenarMatriz(Obj[][] m) {
-        for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m.length; j++) {
-                Obj o = new Obj();
-                o.setDato((int) (Math.random() * 100 + 1));
-                m[i][j] = o;
-
-            }
-        }
-        return m;
-    }
-
-    public void MostrarMatriz(Obj[][] m) {
-        System.out.println("mostremos la matriz");
-        for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m.length; j++) {
-                System.out.print(m[i][j].getDato() + " - ");
-            }
-            System.out.println();
-        }
-    }
-
     public ObjVillalobos[][] LLenarMatriz(ObjVillalobos[][] m, Scanner sc) {
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m.length; j++) {
                 ObjVillalobos o = new ObjVillalobos();
-                System.out.println("Ingrese la descripcion");
-                o.setDescripcion(sc.next());
-                System.out.println("ingrese la cantidad ");
-                o.setCantidad(sc.nextInt());
+                System.out.println("Ingrese el Nombre");
+                o.setNombre(sc.next());
+                System.out.println("ingrese el Stock ");
+                o.setStock(sc.nextInt());
                 System.out.println("Ingrese el precio ");
                 o.setPrecio(sc.nextDouble());
                 m[i][j] = o;
@@ -62,8 +40,8 @@ public class Metodos {
     public void MostrarMatriz(ObjVillalobos[][] m) {
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m.length; j++) {
-                System.out.println("Descripcion: " + m[i][j].getDescripcion());
-                System.out.println("Cantidad: " + m[i][j].getCantidad());
+                System.out.println("Nombre: " + m[i][j].getNombre());
+                System.out.println("Stock: " + m[i][j].getStock());
                 System.out.println("Precio: " + m[i][j].getPrecio());
                 System.out.println("'----------------------------------------- '\n");
             }
@@ -76,8 +54,8 @@ public class Metodos {
             for (int j = 0; j < m.length; j++) {
                 for (int i1 = 0; i1 < m.length; i1++) {
                     for (int j1 = 0; j1 < m.length; j1++) {
-                        if (m[i][j].getDescripcion().equalsIgnoreCase(m2[i1][j1].getDescripcion())) {
-                            m[i][j].setCantidad(m[i][j].getCantidad() + m2[i1][j1].getCantidad());
+                        if (m[i][j].getNombre().equalsIgnoreCase(m2[i1][j1].getNombre())) {
+                            m[i][j].setStock(m[i][j].getStock() + m2[i1][j1].getStock());
                         }
                     }
                 }
@@ -85,8 +63,8 @@ public class Metodos {
         }
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m.length; j++) {
-                m[i][j].setCantidad(m[i][j].getCantidad());
-                m[i][j].setDescripcion(m[i][j].getDescripcion());
+                m[i][j].setStock(m[i][j].getStock());
+                m[i][j].setNombre(m[i][j].getNombre());
                 m[i][j].setPrecio(m[i][j].getPrecio());
             }
         }
